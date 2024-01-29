87cfe2943c74c3d5bd89ec7869d0d9178b0a7b6e
Shant Dashjian <shantgdashjian@gmail.com>
2024-01-29 17:14:23 +0400

    Add basic UI using Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-function App() {
+import {
+  AppBar,
+  Container,
+  CssBaseline,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 
+function App() {
   return (
-    <>
-      Hello, Car Shop!
-    </>
-  )
+    <Container maxWidth="x1">
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">Car Shop</Typography>
+        </Toolbar>
+      </AppBar>
+    </Container>
+  );
 }
 
-export default App
+export default App;
