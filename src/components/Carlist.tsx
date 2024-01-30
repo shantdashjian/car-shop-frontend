6d796ff79d5b3284c1217f93045056de13877bcb
Shant Dashjian <shantgdashjian@gmail.com>
2024-01-30 22:05:30 +0400

    Add paging, filtering, and sorting using DataGrid

--- src/components/Carlist.tsx
+++ src/components/Carlist.tsx
@@ -1,8 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
-import { CarResponse } from "../types";
 import { getCars } from "../api/carapi";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 
 function Carlist() {
+  const columns: GridColDef[] = [
+    { field: "brand", headerName: "Brand", width: 200 },
+    { field: "model", headerName: "Model", width: 200 },
+    { field: "color", headerName: "Color", width: 200 },
+    { field: "registrationNumber", headerName: "Reg.nr.", width: 150 },
+    { field: "modelYear", headerName: "Model Year", width: 150 },
+    { field: "price", headerName: "Price", width: 150 },
+  ];
   const { data, error, isSuccess } = useQuery({
     queryKey: ["cars"],
     queryFn: getCars,
@@ -13,20 +21,11 @@ function Carlist() {
     return <span>Error when fetching cars...</span>;
   } else {
     return (
-      <table>
-        <tbody>
-          {data.map((car: CarResponse) => (
-            <tr key={car._links.self.href}>
-              <td>{car.brand}</td>
-              <td>{car.model}</td>
-              <td>{car.color}</td>
-              <td>{car.registrationNumber}</td>
-              <td>{car.modelYear}</td>
-              <td>{car.price}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <DataGrid
+        rows={data}
+        columns={columns}
+        getRowId={(row) => row._links.self.href}
+      />
     );
   }
 }
