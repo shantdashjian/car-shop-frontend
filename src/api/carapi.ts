7e3e2718e9d87e00dab31dc5e4f183526f559851
Shant Dashjian <shantgdashjian@gmail.com>
2024-01-31 17:40:29 +0400

    Refactor config

--- src/api/carapi.ts
+++ src/api/carapi.ts
@@ -1,45 +1,32 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 import { Car, CarEntry, CarResponse } from "../types";
 
+const getAxiosConfig = (): AxiosRequestConfig => {
+  const token = sessionStorage.getItem("jwt");
+  return {
+    headers: {
+      Authorization: token,
+      "Content-Type": "application/json",
+    },
+  };
+};
+
 export const getCars = async (): Promise<CarResponse[]> => {
-  const token = sessionStorage.getItem('jwt')
-  const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/cars`, {
-    headers: { 'Authorization' : token }
-  });
+  const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/cars`, getAxiosConfig());
   return response.data._embedded.cars;
 };
 
 export const deleteCar = async (link: string): Promise<CarResponse> => {
-  const token = sessionStorage.getItem('jwt')
-  const response = await axios.delete(link, {
-    headers: { 'Authorization' : token }
-  });
+  const response = await axios.delete(link, getAxiosConfig());
   return response.data;
 };
 
 export const addCar = async (car: Car): Promise<CarResponse> => {
-  const token = sessionStorage.getItem('jwt')
-  const response = await axios.post(
-    `${import.meta.env.VITE_API_URL}/api/cars`,
-    car,
-    {
-      headers: {
-        "Content-Type": "application/json",
-        'Authorization' : token
-      },
-    }
-  );
-
+  const response = await axios.post(`${import.meta.env.VITE_API_URL}/api/cars`, car, getAxiosConfig());
   return response.data;
 };
 
 export const updateCar = async (carEntry: CarEntry): Promise<CarResponse> => {
-  const token = sessionStorage.getItem('jwt')
-  const response = await axios.put(carEntry.url, carEntry.car, {
-    headers: {
-      "Content-Type": "application/json",
-      'Authorization' : token
-    },
-  });
+  const response = await axios.put(carEntry.url, carEntry.car, getAxiosConfig());
   return response.data;
 };
