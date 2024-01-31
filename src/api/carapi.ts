4b0b8a5dfcc50fadb8cc577e8d1a20263610c496
Shant Dashjian <shantgdashjian@gmail.com>
2024-01-31 17:33:15 +0400

    Add JWT authentication to all API calls

--- src/api/carapi.ts
+++ src/api/carapi.ts
@@ -2,22 +2,30 @@ import axios from "axios";
 import { Car, CarEntry, CarResponse } from "../types";
 
 export const getCars = async (): Promise<CarResponse[]> => {
-  const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/cars`);
+  const token = sessionStorage.getItem('jwt')
+  const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/cars`, {
+    headers: { 'Authorization' : token }
+  });
   return response.data._embedded.cars;
 };
 
 export const deleteCar = async (link: string): Promise<CarResponse> => {
-  const response = await axios.delete(link);
+  const token = sessionStorage.getItem('jwt')
+  const response = await axios.delete(link, {
+    headers: { 'Authorization' : token }
+  });
   return response.data;
 };
 
 export const addCar = async (car: Car): Promise<CarResponse> => {
+  const token = sessionStorage.getItem('jwt')
   const response = await axios.post(
     `${import.meta.env.VITE_API_URL}/api/cars`,
     car,
     {
       headers: {
         "Content-Type": "application/json",
+        'Authorization' : token
       },
     }
   );
@@ -26,9 +34,11 @@ export const addCar = async (car: Car): Promise<CarResponse> => {
 };
 
 export const updateCar = async (carEntry: CarEntry): Promise<CarResponse> => {
+  const token = sessionStorage.getItem('jwt')
   const response = await axios.put(carEntry.url, carEntry.car, {
     headers: {
       "Content-Type": "application/json",
+      'Authorization' : token
     },
   });
   return response.data;
